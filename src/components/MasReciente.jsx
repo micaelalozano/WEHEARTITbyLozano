77c13fcb5f691d7f7d296abc8d14a6b0e767b3a4
micaelalozano <micaelalozano.16@hotmail.com>
2023-05-02 18:20:21 -0300

    Adding latest update to Git

---
+++ src/components/MasReciente.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import eyes from "../assets/eyes.jpg";
+import golden from "../assets/golden.jpeg";
+import ring from "../assets/ring.jpg";
+import tattoo from "../assets/tattoo.jpg";
+import logo from "../assets/profile.png";
+//Estilos
+import "../estilos/masReciente.css";
+
+const MasReciente = () => {
+  return (
+    <>
+      <div className="contenedor-recientes">
+        <div className="contenedor-uno">
+          <img className="img-art" src={eyes} alt="Pic" />
+          <div className="reciente-texto">
+            <p className="p-1">The unwanted lover</p>
+            <p className="p-2">
+              You've been living in my mind free lately, It's been a while since
+              that day, What can I do about it when I'm not the one.
+            </p>
+            <p className="p-3">Leer más ›</p>
+          </div>
+          <div className="cont-dos">
+            <img className="logo-recent" src={logo} alt="+" />
+            <p className="hearts">500 HEARTS</p>
+          </div>
+        </div>
+
+        <div className="contenedor-uno">
+          <img className="img-art" src={golden} alt="Pic" />
+          <div className="reciente-texto">
+            <p className="p-1">Expressions of love in Arabic</p>
+            <p className="p-2">
+              Hello everyone, I have begun studying Arabic for the past month.
+              And I think Arabic is a beautiful language and i just wanted to
+              share some finding...
+            </p>
+            <p className="p-3">Leer más ›</p>
+          </div>
+          <div className="cont-dos">
+            <img className="logo-recent" src={logo} alt="+" />
+            <p className="hearts">315 HEARTS</p>
+          </div>
+        </div>
+
+        <div className="contenedor-uno">
+          <img className="img-art" src={ring} alt="Pic" />
+          <div className="reciente-texto">
+            <p className="p-1">Motivation vs. Discipline</p>
+            <p className="p-2">
+              Motivation cannot sustain you for long. motivation fades. it comes
+              and goes based on moods and feelings.
+            </p>
+            <p className="p-3">Leer más ›</p>
+          </div>
+          <div className="cont-dos">
+            <img className="logo-recent" src={logo} alt="+" />
+            <p className="hearts">723 HEARTS</p>
+          </div>
+        </div>
+
+        <div className="contenedor-uno">
+          <img className="img-art" src={tattoo} alt="Pic" />
+          <div className="reciente-texto">
+            <p className="p-1">Introducing myself in pictures</p>
+            <p className="p-2">
+              Hello lovely people of WHI! It's been a while since I made an
+              article, but after finding this simple, pretty one I got inspired
+              and..
+            </p>
+            <p className="p-3">Leer más ›</p>
+          </div>
+          <div className="cont-dos">
+            <img className="logo-recent" src={logo} alt="+" />
+            <p className="hearts">188 HEARTS</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MasReciente;
