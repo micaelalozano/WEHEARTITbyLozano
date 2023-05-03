0ab67ae329b99a8aabc27f87f380bc6739d2d521
micaelalozano <micaelalozano.16@hotmail.com>
2023-05-03 17:27:13 -0300

    Adding latest update to Git

--- src/components/NavCelular.jsx
+++ src/components/NavCelular.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DensityMediumIcon from "@mui/icons-material/DensityMedium";
+//Estilos
+import "../estilos/navCelular.css";
 
 const NavCelular = () => {
-  return <div>NavCelular</div>;
+  return (
+    <>
+      <div className="navcelular-contenedor">
+        <button className="bot-1">
+          <DensityMediumIcon
+            sx={{ fontSize: 12 }}
+            style={{ color: "#4a4a4a" }}
+          />
+        </button>
+        <input
+          className="buscador"
+          placeholder="¿ Qué es lo que amas ?"
+          type="text"
+        />
+        <button className="bot-2">
+          <MoreVertIcon sx={{ fontSize: 18 }} style={{ color: "#727272" }} />
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default NavCelular;
