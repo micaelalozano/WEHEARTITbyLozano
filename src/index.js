b54e2ae3ceedd16ca29c27e7c710239f8aa42a1b
micaelalozano <micaelalozano.16@hotmail.com>
2023-05-22 16:51:11 -0300

    Adding latest update to Git

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import App from "./App";
 import "./index.css";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
+
+ReactDOM.render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <App />
+      </StyletronProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
--- src/pages/EditarPerfil.jsx
+++ src/pages/EditarPerfil.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import NavCelular from "../components/NavCelular";
+import { Notification } from "baseui/notification";
 
 const EditarPerfil = () => {
   return (
     <>
       <Navbar />
       <NavCelular />
+      <Notification>
+      {() => "This is a notification."}
+    </Notification>
     </>
   );
 };
