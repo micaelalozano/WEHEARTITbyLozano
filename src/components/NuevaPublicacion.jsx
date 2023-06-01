ecc7b71758eba3c7084e6c412e5f2eb40e261667
micaelalozano <micaelalozano.16@hotmail.com>
2023-06-01 18:27:16 -0300

    Adding latest update to Git

---
+++ src/components/NuevaPublicacion.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { RiCloseCircleFill } from "react-icons/ri";
+//Estilos
+import "../estilos/nuevaPublicacion.css";
+
+const NuevaPublicacion = ({ cerrarModal }) => {
+  const handleCloseClick = () => {
+    cerrarModal();
+  };
+
+  return (
+    <>
+      <div className="modal-publicacion">
+        <div className="div-close">
+          <RiCloseCircleFill className="close" size={20} color="#ffffff" onClick={handleCloseClick} />{" "}
+        </div>
+        <form className="form-publicacion">
+            <p>Crear nueva publicación</p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default NuevaPublicacion;
